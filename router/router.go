e42db76a557811ad8dcefa40ca8c744d8bb7dc78
milospp <misa01523@gmail.com>
2023-06-22 19:46:39 +0200

    Fixed metrics, added dockerfile

--- router/router.go
+++ router/router.go
@@ -8,10 +8,10 @@ import (
 
 func ConfigureRouter(handler *handler.Handler) *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/host-rating", handler.RateHost).Methods("POST")
-	router.HandleFunc("/api/host-rating/host/{id}", handler.TestHandler).Methods("GET")
-	router.HandleFunc("/api/accomodation-rating", handler.TestHandler).Methods("POST")
-	router.HandleFunc("/api/accomodation-rating/accomodation/{id}", handler.TestHandler).Methods("GET")
+	router.HandleFunc("/api/host-rating", metrics.MetricProxy(handler.RateHost)).Methods("POST")
+	router.HandleFunc("/api/host-rating/host/{id}", metrics.MetricProxy(handler.TestHandler)).Methods("GET")
+	router.HandleFunc("/api/accomodation-rating", metrics.MetricProxy(handler.TestHandler)).Methods("POST")
+	router.HandleFunc("/api/accomodation-rating/accomodation/{id}", metrics.MetricProxy(handler.TestHandler).Methods("GET")
 
 	router.Path("/metrics").Handler(metrics.MetricsHandler())
 
